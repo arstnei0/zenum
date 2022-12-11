44941f4c2a25cb950163b010f34c640c7d1bdb71
Zihan Chen <me@zihan.ga>
2022-12-11 12:23:57 +0800

    Init

---
+++ src/index.ts
@@ -0,0 +1,36 @@
+type Matchers<T, R = any> =
+	| {
+			[K in keyof T]: (data: T[K]) => R
+	  }
+	| ({
+			[K in keyof T]?: (data: T[K]) => R
+	  } & {
+			_: (data: T[keyof T]) => R
+	  })
+
+type Zitem<T, K extends keyof T = keyof T> = { _: K; $: T[K] }
+
+class Zenum<T extends Record<string | symbol, any>> {
+	match<K extends keyof T, R>(item: Zitem<T, K>, matchers: Matchers<T, R>) {
+		return (matchers[item._] || matchers._)(item.$)
+	}
+
+	item<K extends keyof T>(k: K, data: T[K]): Zitem<T, K> {
+		return { _: k, $: data }
+	}
+}
+
+type ZenumFactory<T> = InstanceType<typeof Zenum<T>> & {
+	[K in keyof T]: (data: Zitem<T, K>["$"]) => Zitem<T, K>
+}
+
+export const zenum = <T>(): ZenumFactory<T> => {
+	const zenum = new Zenum<T>()
+	const proxy = new Proxy(zenum, {
+		get<K>(target, p: K, receiver) {
+			return target[p] || ((data: T[keyof T]) => target.item(p, data))
+		},
+	}) as ZenumFactory<T>
+
+	return proxy
+}
