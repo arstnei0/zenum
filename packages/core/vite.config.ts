33a568d04e052a0ec00dc38315edbdabab8f680d
Zihan Chen <me@zihan.ga>
2022-12-13 18:49:31 +0800

    Update package.jsons

--- packages/core/vite.config.ts
+++ packages/core/vite.config.ts
@@ -1,11 +1,10 @@
 import { defineConfig } from "vite"
 import dts from "vite-plugin-dts"
-import fg from 'fast-glob'
 
 export default defineConfig({
 	build: {
 		lib: {
-			entry: ["src/index.ts", 'src/dev.ts'],
+			entry: ["src/index.ts"],
 			formats: ["es"],
 		},
 		rollupOptions: {
